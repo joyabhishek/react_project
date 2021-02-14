2db76320fc92f15935f4daa9655c4aff0bf1ae31
joyAbhishek <abhirocz037@gmail.com>
2021-02-14 08:28:03 +0530

    Another commit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
 import TodoItem from "./TodoItem"
 import "./style.css"
 import todosData from "./todosData"
 
-function App()
+class App extends Component
 {
-    let todosHTML = todosData.map(function (todo) {
-        return (
-            <TodoItem 
-            key = {todo.id}    
-            todoText = {todo.text}
-            completed = {todo.completed}
-                />
-        )
+    constructor(){
+        super()
+        this.state = {todosData}
+    }
 
-    })
-    return(
-        <div>
-            {todosHTML}
-        </div>
-    )
+    render (){
+        console.log(this.state)
+        let todosHTML = this.state.todosData.map(function (todo) {
+            return (
+                <TodoItem 
+                key = {todo.id}    
+                todoText = {todo.text}
+                completed = {todo.completed}
+                />
+            )
+    
+        })
+        return(
+            <div>
+                {todosHTML}
+            </div>
+        )
+    }
+
 }
 
 export default App
